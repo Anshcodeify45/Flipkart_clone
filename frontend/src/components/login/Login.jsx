9e7944c615147630b145ba0b4ceac51616bebf74
Anshcodeify45 <anishpatnaik45@gmail.com>
2024-02-27 12:03:12 +0530

    Day-4

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Box , TextField ,Typography , Button , Dialog , styled} from '@mui/material';
-
+import { useState } from 'react';
 
 
 const Component = styled(Box)`
@@ -12,8 +12,8 @@ padding-top: 0;
 
 const Poster = styled(Box)`
 background: #2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85% no-repeat;
-width: 40%;
-height: 100%;
+width: 28%;
+height: 82.5%;
 padding: 45px 35px;
 & > p, & > h5 {
     color: #FFFFFF;
@@ -21,27 +21,99 @@ padding: 45px 35px;
 }
 `
 
+const Wrapper = styled(Box)`
+display : flex;
+flex-direction : column;
+padding: 25px 35px;
+flex:1;
+& > div , & > button , & > p{
+    margin-top :20px;
+}
+`
+const LoginBtn = styled(Button)`
+text-transform : none;
+background:#FB641B;
+color:#fff;
+height: 48px;
+border-radius : 2px;
+`
+
+const RequestOtp = styled(Button)`
+text-transform : none;
+bacgroung:#fff;
+color:#2874f0;
+height: 48px;
+border-radius : 2px;
+box-shadow : 0 2px 4px 0 rgb(0 0 0/ 20%);
+`
+
+const Text = styled(Typography)`
+font-size : 16px;
+color:#878787;
+`
+
+const CreateAccount = styled(Typography)`
+font-size : 14px;
+text-align:center;
+color:#2874f0;
+font-weight:600;
+cursor : pointer;
+`
+const accountInitial = {
+    login : {
+        view:'login', 
+        heading: 'Login',
+        subHeading:'Get access to your Orders , Wishlist and Recomendations'
+    },
+    signup:{
+        view : 'signup',
+        heading: "Looks like you're new here!" ,
+        subHeading : 'Sign up with your mobile number to get started',
+    }
+}
 function Login({open ,setOpen}) {
+
+    const [LoginAcnt , Signupacnt] = useState(accountInitial.login)
+
+
+    const togglesignup = () => {
+        Signupacnt(accountInitial.signup)
+    } 
 
     const offLogin = () => {
         setOpen(false);
+        Signupacnt(accountInitial.login);
     }
   return (
-    <Dialog open={open} onClose={offLogin}>
+    <Dialog open={open} onClose={offLogin} PaperProps={{sx : {maxWidth: 'unset'}}} >
         <Component style={{display:'flex'}}>
             <Poster>
-            <Typography variant="h5">Login</Typography>
-            <Typography style={{marginTop: 20}}>Get access to your Orders , Wishlist and Recomendations</Typography>
+            <Typography variant="h5">{LoginAcnt.heading}</Typography>
+            <Typography style={{marginTop: 20}}>{LoginAcnt.subHeading}</Typography>
             </Poster>
-            <Box>
+           {
+            LoginAcnt.view === 'login' ?
+                <Wrapper>
                 <TextField variant='standard' label = 'Enter Email/Mobile number' />
                 <TextField variant='standard' label = 'Enter Password' />
-                <TextField>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy</TextField>
-                <Button>Login</Button>
-                <Typography>OR</Typography>
-                <Button>Request OTP</Button>
-                <Typography>New to Flipkart? Create an account</Typography>
-            </Box>
+                <Text>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy</Text>
+                <LoginBtn>Login</LoginBtn>
+                <Typography style={{textAlign:'center'}}>OR</Typography>
+                <RequestOtp>Request OTP</RequestOtp>
+                <CreateAccount onClick={() => togglesignup()}>New to Flipkart? Create an account</CreateAccount>
+                </Wrapper>
+            :
+                <Wrapper>
+                <TextField variant='standard' label = 'Enter Firstname' />
+                <TextField variant='standard' label = 'Enter Lastname' />
+                <TextField variant='standard' label = 'Enter Username' />
+                <TextField variant='standard' label = 'Enter Email' />
+                <TextField variant='standard' label = 'Enter Password' />
+                <TextField variant='standard' label = 'Enter Phone' />
+                <LoginBtn>Continue</LoginBtn>
+                
+                </Wrapper>
+           } 
 
         </Component>
       
